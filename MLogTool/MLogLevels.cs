e5ffba2c109cc980c5696c2530de30a53fd6b2e2
PositiveMumu <positivemumu@126.com>
2020-11-10 23:59:34 +0800

    日志工具 版本：v0.1

---
+++ MLogTool/MLogLevels.cs
@@ -0,0 +1,23 @@
+﻿// ****************************************************
+// 	文件：MLogLevels.cs
+// 	作者：积极向上小木木
+// 	邮箱：PositiveMumu@126.com
+// 	日期：2020/11/10 20:36:53
+// 	功能：日志等级枚举
+// *****************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace MTools.MLogTool
+{
+    public enum MLogLevels
+    {
+        Log = 0,
+        Information = 1,
+        Warning = 2,
+        Error = 3
+    }
+}
